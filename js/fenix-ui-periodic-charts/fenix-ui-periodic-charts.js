75098c59b30efebd1cdfc406330bf82a9fef3647
guidofenix <guido.barbaglia@fao.org>
2014-10-07 14:17:55 +0200

    Add time series to the chart.

--- js/fenix-ui-periodic-charts/fenix-ui-periodic-charts.js
+++ js/fenix-ui-periodic-charts/fenix-ui-periodic-charts.js
@@ -1,27 +1,93 @@
 define(['jquery',
         'mustache',
+        'text!fenix-ui-periodic-charts/json/chart_template.json',
         'highcharts',
-        'highcharts_exporting'], function ($, Mustache) {
+        'highcharts_exporting'], function ($, Mustache, chart_template, highcharts) {
 
     'use strict';
 
-    function FENIX_UI_PERIODIC_CHARTS() {
+    function FENIX_UI_PERIODIC_CHARTS(config) {
 
+        /* Module's configuration. */
         this.CONFIG = {
-
+            render_to: null,
+            periodic_series_name: null,
+            periodic_series_values: [],
+            time_series_name: null,
+            time_series_values: [],
+            default_colors: ['#379bcd', '#76BE94', '#744490', '#E10079', '#2D1706', '#F1E300', '#F7AE3C', '#DF3328']
         };
-
-    }
-
-    FENIX_UI_PERIODIC_CHARTS.prototype.init = function(config) {
 
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
-        alert('welcome');
+        /* Cast configuration files to JSON. */
+        chart_template = $.parseJSON(chart_template);
+
+    }
+
+    FENIX_UI_PERIODIC_CHARTS.prototype.add_periodic_series = function(name, values) {
+        this.CONFIG.periodic_series_values = values;
+        this.CONFIG.periodic_series_name = name;
+    };
+
+    FENIX_UI_PERIODIC_CHARTS.prototype.add_time_series = function(name, values) {
+        this.CONFIG.time_series_values = values;
+        this.CONFIG.time_series_name = name;
+    };
+
+    FENIX_UI_PERIODIC_CHARTS.prototype.plot = function() {
+        this.init_plot();
+    };
+
+    FENIX_UI_PERIODIC_CHARTS.prototype.init_plot = function() {
+
+        /* This... */
+        var _this = this;
+
+        /* Load chart template. */
+        var template_payload = chart_template;
+
+        /* Add load function. Data will be plottetd after chart's rendering. */
+        var custom_payload = {
+            chart: {
+                events: {
+                    load: function () {
+                        _this.add_series_to_the_plot(this.series);
+                    }
+                }
+            },
+            colors: this.CONFIG.default_colors
+        };
+
+        /* Initialize series. */
+        custom_payload.series = [];
+
+        /* Series 0: periodic series. */
+        custom_payload.series[0] = {};
+
+        /* Series 1: time series. */
+        custom_payload.series[1] = {};
+
+        /* Merge the template and the custom settings. */
+        template_payload = $.extend(true, {}, template_payload, custom_payload);
+
+        /* Render the (empty) chart. */
+        $('#' + this.CONFIG.render_to).highcharts(template_payload);
 
     };
 
-    return new FENIX_UI_PERIODIC_CHARTS();
+    FENIX_UI_PERIODIC_CHARTS.prototype.add_series_to_the_plot = function(series) {
+
+        /* Add the time series to the chart. */
+        series[1].setData(this.CONFIG.time_series_values);
+        series[1].update({name: this.CONFIG.time_series_name}, true);
+
+        series[0].setData(this.CONFIG.periodic_series);
+
+    };
+
+    /* Return the module. */
+    return FENIX_UI_PERIODIC_CHARTS;
 
 });
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ require.config({
 
     paths: {
         text:                       'libs/text',
-        i18n:                       'common/libs/i18n',
+        i18n:                       'libs/i18n',
         jquery:                     '//fenixapps.fao.org/repository/js/jquery/1.10.2/jquery-1.10.2.min',
         mustache:                   '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache',
         highcharts:                 '//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
@@ -16,11 +16,57 @@ require.config({
         },
         highcharts_exporting: {
             deps: ['highcharts']
+        },
+        FENIX_UI_PERIODIC_CHARTS: {
+            deps: [
+                'text',
+                'i18n',
+                'jquery',
+                'mustache',
+                'highcharts',
+                'highcharts_exporting'
+            ]
         }
     }
 
 });
 
 require(['FENIX_UI_PERIODIC_CHARTS'], function (FENIX_UI_PERIODIC_CHARTS) {
-    FENIX_UI_PERIODIC_CHARTS.init({});
+    var module = new FENIX_UI_PERIODIC_CHARTS({
+        render_to: 'chart_placeholder'
+    });
+    module.add_time_series('This is the time series', [
+        [Date.UTC(1982, 6,  1), 3.14],
+        [Date.UTC(1982, 6,  2), 2.78],
+        [Date.UTC(1982, 6,  3), 4.75],
+        [Date.UTC(1982, 6,  4), 3.75],
+        [Date.UTC(1982, 6,  5), 6.75],
+        [Date.UTC(1982, 6,  6), 4.75],
+        [Date.UTC(1982, 6,  7), 7.75],
+        [Date.UTC(1982, 6,  8), 8.75],
+        [Date.UTC(1982, 6,  9), 2.75],
+        [Date.UTC(1982, 6, 10), 7.75],
+        [Date.UTC(1982, 6, 11), 3.75],
+        [Date.UTC(1982, 6, 12), 7.75],
+        [Date.UTC(1982, 6, 13), 4.75],
+        [Date.UTC(1982, 6, 14), 9.75],
+        [Date.UTC(1982, 6, 15), 3.75],
+        [Date.UTC(1982, 6, 16), 4.75],
+        [Date.UTC(1982, 6, 17), 9.75],
+        [Date.UTC(1982, 6, 18), 5.75],
+        [Date.UTC(1982, 6, 19), 9.75],
+        [Date.UTC(1982, 6, 20), 2.75],
+        [Date.UTC(1982, 6, 21), 8.75],
+        [Date.UTC(1982, 6, 22), 0.75],
+        [Date.UTC(1982, 6, 23), 5.75],
+        [Date.UTC(1982, 6, 24), 2.75],
+        [Date.UTC(1982, 6, 25), 8.75],
+        [Date.UTC(1982, 6, 26), 0.75],
+        [Date.UTC(1982, 6, 27), 3.75],
+        [Date.UTC(1982, 6, 28), 7.75],
+        [Date.UTC(1982, 6, 29), 0.75],
+        [Date.UTC(1982, 6, 30), 3.75],
+        [Date.UTC(1982, 6, 31), 5.75]
+    ]);
+    module.plot();
 });
